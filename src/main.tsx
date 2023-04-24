2da93f50b9ebade7c70ca9cd7c914708a8fd2b38
Felipe-SN <felipewfk@gmail.com>
2023-04-24 10:04:12 -0300

    refatorado:   mudança de Routes para RoutesProvider para evitar conflitos

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client';
 import * as React from 'react';
 import { RecoilRoot } from 'recoil';
 import GlobalStyle from 'components/GlobalStyle';
-import Routes from 'routes';
+import RoutesProvider from 'routes/RoutesProvider';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
       <GlobalStyle />
-      <Routes />
+      <RoutesProvider />
     </RecoilRoot>
   </React.StrictMode>
 );
--- src/routes/RoutesProvider.tsx
+++ src/routes/RoutesProvider.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
-export default function Routes() {
+export default function RoutesProvider() {
   return <RouterProvider router={router} />;
 }
